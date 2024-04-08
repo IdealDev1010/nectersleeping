9c0120589cf4a9f501303811947b3d92d7829380
IdealDev1010 <idealdev1010@gmail.com>
2024-04-08 03:01:04 -0500

    create product content page

--- assets/customer.js
+++ assets/customer.js
@@ -24,13 +24,13 @@ class CustomerAddresses {
     const container = document.querySelector(selectors.customerAddresses);
     return container
       ? {
-          container,
-          addressContainer: container.querySelector(selectors.addressContainer),
-          toggleButtons: document.querySelectorAll(selectors.toggleAddressButton),
-          cancelButtons: container.querySelectorAll(selectors.cancelAddressButton),
-          deleteButtons: container.querySelectorAll(selectors.deleteAddressButton),
-          countrySelects: container.querySelectorAll(selectors.addressCountrySelect),
-        }
+        container,
+        addressContainer: container.querySelector(selectors.addressContainer),
+        toggleButtons: document.querySelectorAll(selectors.toggleAddressButton),
+        cancelButtons: container.querySelectorAll(selectors.cancelAddressButton),
+        deleteButtons: container.querySelectorAll(selectors.deleteAddressButton),
+        countrySelects: container.querySelectorAll(selectors.addressCountrySelect),
+      }
       : {};
   }
 
@@ -85,4 +85,147 @@ class CustomerAddresses {
 }
 
 
-const variantFirmnessArray = variant.map []
+window.addEventListener("load", function () {
+
+  var checkBoxs = document.querySelectorAll('.toggle_switch');
+  var newButton = document.getElementById("compare_variant");
+  var closeButton = document.getElementsByClassName("modal-close-button")[0];
+  var modal = document.getElementById('MyModal');
+  var selectElement1 = document.getElementById('compared-variant-1');
+  var selectElement2 = document.getElementById('compared-variant-2');
+  var selectIndex = 1;
+
+  function updateButtonState() {
+    var checkedCount = 0;
+    checkBoxs.forEach(function (checkBox) {
+      if (checkBox.checked) {
+        checkedCount += 1;
+      }
+    });
+
+    if (checkedCount == 0) {
+        checkBoxs.forEach(function (checkBox) {
+          checkBox.disabled = false;
+        });
+    }
+
+    else if (checkedCount == 1) {
+        newButton.classList.remove('hidden');
+        newButton.classList.add('disabled');
+        newButton.textContent = 'Select one more to compare';
+
+        checkBoxs.forEach(function (checkBox) {
+        checkBox.disabled = false;
+      });
+    }
+
+    else if (checkedCount == 2) {
+
+      newButton.classList.remove('disabled');
+      newButton.classList.remove('hidden');
+      newButton.textContent = 'Compare Selections';
+      
+      checkBoxs.forEach(function (checkBox) {
+        if (!checkBox.checked) {
+          checkBox.disabled = true;
+        }
+
+        if (checkBox.checked) {
+          checkedCount += 1;
+          var variantid = checkBox.id;
+          var optionToselect = selectIndex == 1 ? selectElement1 : selectElement2;
+          for (let i = 0; i < optionToselect.options.length; i++) {
+            console.log(optionToselect.value, 'optionToselect');
+            if (optionToselect.options[i].value == variantid) {
+              optionToselect.options[i].selected = true;
+              break;
+            }
+          }
+          selectIndex++;
+        }
+      });
+
+      const getItems = document.querySelectorAll('.grid__item.container-item');
+      function showSelectedVariantInfo() {
+        console.log('test function')
+        let checkedItems = []
+        for (const item of getItems) {
+          if (item.querySelector('label.switch input')) {
+            if (!item.querySelector('label.switch input').disabled) {
+              checkedItems.push(item.querySelector('img').src);
+            }
+          }
+        }
+        console.log(checkedItems);
+        for (const oneId of checkedItems) {
+          console.log(document.querySelector(`[src="${oneId.replace('https:', '')}"]`).parentElement.parentElement);
+          document.querySelector(`.grid__item.modal-item [src="${oneId.replace('https:', '')}"]`).parentElement.parentElement.classList.remove('hidden');
+        }
+      }
+    }
+
+  }
+  checkBoxs.forEach(element => {
+    element.addEventListener('change', updateButtonState);
+  });
+
+
+  updateButtonState();
+
+  newButton.addEventListener('click', function () {
+    modal.style.display = "block";
+    showSelectedVariantInfo();
+  });
+
+  closeButton.addEventListener('click', function () {
+    modal.style.display = "none";
+  });
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  const getItems = document.querySelectorAll('.grid__item.container-item');
+  function showSelectedVariantInfo() {
+    console.log('test function')
+    let checkedItems = []
+    for (const item of getItems) {
+      if (item.querySelector('label.switch input')) {
+        if (!item.querySelector('label.switch input').disabled) {
+          checkedItems.push(item.getAttribute('data-testid'));
+        }
+      }
+    }
+    
+    for (let i = 0; i < checkedItems.length; i++) {
+      // console.log(document.querySelector(`[src="${oneId.replace('https:', '')}"]`).parentElement.parentElement);
+        document.querySelector(`[data-testid${1}="${checkedItems[0]}"]`).classList.remove('hidden');
+        document.querySelector(`[data-testid${2}="${checkedItems[1]}"]`).classList.remove('hidden');
+    }
+  }
+
+  selectElement1.addEventListener('change', function(event) {
+    const item1Els = document.querySelectorAll('.grid__item.modal-item1');
+    Array.from(item1Els).map((oneItem) => {
+      oneItem.classList.remove('hidden');
+      oneItem.classList.add('hidden');
+    })
+
+    document.querySelector(`[data-testid1="${document.querySelector("select#compared-variant-1").value}"]`).classList.remove('hidden');
+  })
+
+  selectElement2.addEventListener('change', function(event) {
+    const item1Els = document.querySelectorAll('.grid__item.modal-item2');
+    Array.from(item1Els).map((oneItem) => {
+      oneItem.classList.remove('hidden');
+      oneItem.classList.add('hidden');
+    })
+
+    const selectedValue = document.querySelector("select#compared-variant-2").value;
+    document.querySelector(`[data-testid2="${selectedValue}"]`).classList.remove('hidden');
+  })
+});
+
+
---
+++ assets/tab-accordion.js
@@ -0,0 +1,20 @@
+// window.addEventListener("load", (event) => {
+//     document.querySelectorAll('.tab-link').forEach(link => {
+//         link.addEventListener('click', function(e){
+//             e.preventDefault();
+//             document.querySelectorAll('.tab-link').forEach(_link => {
+//                 _link.classList.remove('active');
+//             });
+//             this.classList.add('active');
+//             const tab = this.dataset.tab;
+//             document.querySelectorAll('.tab-content').forEach(content => {
+//                 content.classList.add('hidden');
+//             });
+//             document.querySelector(`.tab-content[data-tab="${tab}"]`).classList.remove('hidden');
+//         });
+//     });
+// });
+
+
+
+
